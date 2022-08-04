e355d2e009e55ef237667f7a75d783ef94bad4bb
saranell <76405224+saranell@users.noreply.github.com>
2022-08-03 20:18:39 -0500

    First commit

---
+++ 2.6.Studio_Area_of_a_Circle/Program.cs
@@ -0,0 +1,20 @@
+﻿// Calculate the Area of a Circle
+Console.WriteLine("Enter a radius: ");
+
+double r = double.Parse (Console.ReadLine());
+double pi = 3.14;
+double area = pi * r * r;
+
+Console.WriteLine($"The area of a circle with radius {r} is: {area}");
+
+double circumference = 2 * pi * r;
+double diameter = 2 * r;
+
+Console.WriteLine($"The circumference is {circumference} & the diameter is {diameter}");
+
+Console.WriteLine("What is the mpg of your car? ");
+
+double mpg = double.Parse(Console.ReadLine());
+double gallons = mpg / diameter;
+
+Console.WriteLine($"It will take {gallons} gallons to go around the circle.");
